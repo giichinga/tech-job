fdf64200f827fb8224beb83dbb79285122d70f10
Tonny <antonnygichinga97@gmail.com>
2024-10-28 22:45:26 +0300

    finished

---
+++ tech-job/src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './Pages/Landing.jsx';
+import About from './Pages/about.jsx'
+import Contact from './Pages/Contact.jsx'
+import Services from './Pages/services.jsx';
+import Hero from './Pages/index.jsx';
+import Error from './Pages/404.jsx';
+import './CSS/App.css';
+
+
+function App() {
+  return(
+   <>
+   <BrowserRouter>
+    <Routes>
+      <Route index element={< Hero/>} />
+      <Route path='/' element={< Hero/>} />
+      <Route path='/home' element={< Hero/>} />
+      <Route path='/search' element={<LandingPage />} />
+      <Route path= '/about' element={<About/>} />
+      <Route path= '/services' element={<Services/>} />
+      <Route path= '/contact' element={<Contact/>} />
+      <Route path= '*' element={<Error/>} />
+    </Routes>
+   </BrowserRouter>
+   </>
+  );
+}
+
+export default App
---
+++ tech-job/src/Pages/Contact.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+
+export default function Contact() {
+    document.title = 'TechSearch - Contact';
+    return (
+        <>
+        <nav className="topNav">
+        <Link to= "/home">
+          <h1>TechSearch</h1>
+        </Link>
+        <ul className="navLinks" >
+            <Link to="/about">
+              <li><a>About</a></li>
+            </Link>
+            <Link to="/services">
+              <li><a>Services</a></li>
+            </Link>
+            <Link to="/contact">
+              <li><a className='thisLink'>Contact</a></li>
+            </Link>
+        </ul>
+    </nav>
+        <div className='mainContent' >
+            <h1>Contact</h1>
+            <p>Feel free to reach out to us at:</p>
+            <p>Phone: 555-555-5555</p>
+            <p>Email: antonnygichinga@gmail.com </p>
+        </div>
+        </>
+
+    );
+
+}
